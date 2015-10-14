b4300d75aac98e7660820f96326ef8f051f4fc58
Gede Wahyu Adi Pramana <gedewahyu.ap@gmail.com>
2015-10-14 10:09:45 +0700

    Init, client-nya ketinggalan

---
+++ Client/src/java/sisdis/Klien.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sisdis;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URL;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author wahyuoi
+ */
+@WebServlet(name = "Klien", urlPatterns = {"/klien"})
+public class Klien extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String url_ = request.getParameter("url");
+        String nama = request.getParameter("nama");
+        
+        URL url = new URL(url_);
+        
+        HelloService helloService = new HelloService(url);
+        
+        HelloPortType helloPort = helloService.getHelloPort();
+        String result = helloPort.hello(nama);
+        
+        request.setAttribute("result", result);
+        request.getRequestDispatcher("form.jsp").forward(request, response);
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("form.jsp").forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ Client/web/form.jsp
@@ -0,0 +1,23 @@
+<%-- 
+    Document   : form
+    Created on : Oct 14, 2015, 9:17:50 AM
+    Author     : wahyuoi
+--%>
+<% String result = (request.getAttribute("result")==null)?"":(String)request.getAttribute("result"); %>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <form action="/tugas3/klien" method="POST">
+            URL WSDL : <input type="text" name="url"><br>
+            String Dikitim : <input type="text" name="nama"><br>
+            <input type="submit" value="Kirim">
+            <br><br>
+            <h4><%= result %></h4>
+        </form>
+    </body>
+</html>
